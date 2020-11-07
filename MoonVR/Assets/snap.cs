b000a67cbbcf27c09c567d2a62b392df9be450d2
lauren_brady <laurenbrady01@tamu.edu>
2020-11-06 20:28:05 -0600

    makes the connectors snap

---
+++ MoonVR/Assets/snap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snap : MonoBehaviour
+{ 
+
+    public GameObject maleConnector;
+    public GameObject femaleConnector;
+
+
+
+    void Update()
+    {
+        float dist = Vector3.Distance(maleConnector.transform.position, femaleConnector.transform.position);
+        Debug.Log(dist);
+
+        if ((0 < dist  && dist < 0.12) | (dist < 0 && dist > -0.12))
+        {
+            maleConnector.transform.position = femaleConnector.transform.position;
+            maleConnector.transform.rotation = femaleConnector.transform.rotation;
+        }
+
+
+    }
+
+
+}
