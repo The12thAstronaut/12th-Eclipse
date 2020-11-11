30f9632ea8c10b5d7c9a0e5f77d6b606e1ed60f7
lauren_brady <laurenbrady01@tamu.edu>
2020-11-11 15:21:53 -0600

    changed the experiment panels to have current step view

---
+++ MoonVR/Assets/minimize.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class minimize : MonoBehaviour
+{
+
+
+    public Interactable minimizeButton;
+    public GameObject bigPanel;
+    public GameObject littlePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minimizeButton.OnClick.AddListener(() => minimizePanel());
+        littlePanel.SetActive(false);
+    }
+
+    public void minimizePanel()
+    {
+        
+        bigPanel.SetActive(false);
+        littlePanel.SetActive(true);
+    }
+}
