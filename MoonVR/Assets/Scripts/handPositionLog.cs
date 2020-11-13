beb2268ce9b1f8536de3feff84dd74e2f1fd3458
Neil McHenry <neilmchenry@gmail.com>
2020-11-12 19:35:03 -0600

    Created handPositionLog.cs

---
+++ MoonVR/Assets/Scripts/handPositionLog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handPositionLog : MonoBehaviour
+{
+
+    public GameObject rightHand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            rightHand = GameObject.Find("IndexTip Proxy Transform");
+            Debug.Log("A pressed");
+        }
+
+        if(rightHand != null)
+        {
+            Debug.Log(rightHand.transform.position);
+        }
+
+    }
+}
